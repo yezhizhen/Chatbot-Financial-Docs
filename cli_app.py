ca1dbd2fbe1d5577784f16ccdb1a04552498c854
Euclid <yezhizhenjiakang@gmail.com>
2023-05-10 21:33:28 +0800

    Enabled parser for switching source tracking. Updated requirements.txt

--- cli_app.py
+++ cli_app.py
@@ -3,10 +3,22 @@ from query_data import get_chain
 from constants import *
 import dotenv
 from os import path
+import argparse
+
 dotenv.load_dotenv()
 
 
+def pre_process():
+    parser = argparse.ArgumentParser(description='A cli app for interacting with financial documents')
+    parser.add_argument('--source', action='store_true', help='enable source tracking')
+    args = parser.parse_args()
+    return args.source
+
 if __name__ == "__main__":
+    history = pre_process()
+    if history:
+        print("Source enabled!")
+
     with open(path.join('embedded_store', STORE_NAME + ".pkl"), "rb") as f:
         vectorstore = pickle.load(f)
     qa_chain = get_chain(vectorstore)
@@ -19,4 +31,6 @@ if __name__ == "__main__":
         print("AI:")
         print(result["answer"])
         print("")
-        print(f"Above result is from documents: {[path.split(doc.metadata['source'])[1]  for doc in result['source_documents']]}\n")
+        if history:
+            print(f"Above result is from documents: {[path.split(doc.metadata['source'])[1]  for doc in result['source_documents']]}\n")
+        print("------")
--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 MAX_HISTORY_LEN = 4
 STORE_NAME = "MSFT_filing"
-FOLDER_PATH = r"..\MSFT\Transcripts"
+FOLDER_PATH = r"../MSFT/Filing All"
 STORES = ['MSFT_filing','MSFT_transcripts']
