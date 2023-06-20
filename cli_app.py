ee5a6a9734658fd7f20584e7666a18be7c82c2a2
Euclid <yezhizhenjiakang@gmail.com>
2023-06-21 01:59:19 +0800

    Parser: added parameter for vectorstore, years, initial # of docs

--- cli_app.py
+++ cli_app.py
@@ -12,16 +12,22 @@ dotenv.load_dotenv()
 def pre_process():
     parser = argparse.ArgumentParser(description='A app for interacting with pre compiled documents')
     parser.add_argument('--source', action='store_true', help='enable source tracking')
+    parser.add_argument('-n', help="Filter Retrieved documents, within n years earlier than the latest returned", default=0, type=int)
+    parser.add_argument('-v',"--vectorstore", default=STORE_NAME, type=str, help="index to be used")
+    parser.add_argument('-k', default=6, type=int, help="number of initial documents retrieved")
     args = parser.parse_args()
-    return args.source
+    
+    return args
 
 if __name__ == "__main__":
-    history = pre_process()
+    args = pre_process()
+    history = args.source
     if history:
         print("Source enabled!")
-
-    vectorstore = FAISS.load_local(path.join('embedded_store', STORE_NAME), OpenAIEmbeddings())
-    qa_chain = get_chain(vectorstore)
+    
+    print(f"Using vectorestore {args.vectorstore}, looking back {args.n} years earlier than the latest found, among {args.k} initial docs")
+    vectorstore = FAISS.load_local(path.join('embedded_store', args.vectorstore), OpenAIEmbeddings())
+    qa_chain = get_chain(vectorstore, args.n, args.k)
     #limit the length of history
     print(WELCOME_MSG)
     while True:
--- query_data.py
+++ query_data.py
@@ -21,16 +21,18 @@ prompt = PromptTemplate(template=template, input_variables=["context", "question
 # question_generator needs the openAI.
 
 #a wrapper around default retriever, with priority on latest
-class date_ordered_retriever(BaseRetriever):
+#return documents in latest the year among results
+class latest_n_year_retriever(BaseRetriever):
     retriever: VectorStoreRetriever
     date_pattern = re.compile(r'(\d+).txt')
     
     def get_date(self, doc):
         return self.date_pattern.search(doc.metadata["source"]).group(1) 
 
-    def __init__(self, _retriever) -> None:
+    def __init__(self, _retriever, _n) -> None:
         super().__init__()
         self.retriever = _retriever
+        self.n = _n
     
     def get_relevant_documents(self, query: str):
         initial_docs = self.retriever.get_relevant_documents(query)
@@ -39,11 +41,12 @@ class date_ordered_retriever(BaseRetriever):
             return self.get_date(doc)
 
         initial_docs.sort(key= key_func, reverse= True)
+        
         #return those with highest years 
         latest_year = self.get_date(initial_docs[0])[:4]
         
         for i in range(1, len(initial_docs)):
-            if self.get_date(initial_docs[i])[:4] != latest_year:
+            if int(self.get_date(initial_docs[i])[:4]) < int(latest_year) - self.n:
                 return initial_docs[:i]
         
         return initial_docs
@@ -53,16 +56,21 @@ class date_ordered_retriever(BaseRetriever):
         raise NotImplementedError
 
 
-def get_chain(vectorstore):
+#combine_docs in qa_chain needs the openAI.
+
+#return documents in latest the year among results
+
+
+def get_chain(vectorstore, n = 0, k = 6):
     qa_chain = ConversationalRetrievalChain.from_llm(
         OpenAI(temperature=0),
         #default retriever: vectorstore.as_retriever()
-        date_ordered_retriever(vectorstore.as_retriever()),
+        latest_n_year_retriever(vectorstore.as_retriever(search_kwargs={"k":k}), n),
         #remove memory and pass in "chat_history" when calling for manual passing
         memory = ConversationBufferWindowMemory(k = MAX_HISTORY_LEN, memory_key="chat_history", return_messages=True, output_key='answer'),
         return_source_documents=True,
         combine_docs_chain_kwargs={"prompt": prompt},
-        verbose= True
+        #verbose= True
     )
-    qa_chain.max_tokens_limit = 4095
+    qa_chain.max_tokens_limit = 4096
     return qa_chain
