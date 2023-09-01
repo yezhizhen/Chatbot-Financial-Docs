178131826a9c6a0077f710dc2dd96d53675cfa76
Euclid <yezhizhenjiakang@gmail.com>
2023-09-01 10:20:00 +0800

    Front End Portal V2: working release with chat

---
+++ Front End OCBC/chat_core.py
@@ -0,0 +1,107 @@
+import streamlit as st
+
+import sys
+
+
+
+@st.cache_data
+def add_path():
+    import dotenv
+    # to be changed
+    dotenv.load_dotenv()
+    sys.path.insert(0, ".")
+
+
+add_path()
+
+import openai
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+from os import path
+import json
+
+
+ 
+
+
+# the relative path to the python project path. "Front End OCBC" in this case
+
+
+@st.cache_resource
+def get_vectorstore(market):  # n, k
+    # print(f"Using vectorestore {args.vectorstore}, looking back {args.n} years earlier than the latest found, among {args.k} closest docs")
+    print(f"Getting vectorstore in {market}")
+
+    with open(
+        path.join(st.session_state.relative_dir_name, "market_to_index.json"), "r"
+    ) as f:
+        index_name = json.load(f)[market.lower()]
+    vectorstore = FAISS.load_local(
+        path.join("embedded_store", index_name), OpenAIEmbeddings()
+    )
+    return vectorstore
+    # qa_chain = get_chain(vectorstore, args.n, args.k)
+
+@st.cache_resource
+def get_retriever(window_length_of_years, num_initial_docs, market):
+    from query_data import latest_n_year_retriever
+    print(f"Getting retriever with window length: {window_length_of_years}, docs limit: {num_initial_docs}, in {market}")    
+    vectorstore = get_vectorstore(market)
+    return latest_n_year_retriever(vectorstore.as_retriever(search_kwargs={"k":num_initial_docs}), window_length_of_years)
+
+@st.cache_resource
+def get_qa_chain(market, window_length_of_years, num_initial_docs):
+    print(f"Getting chain with window length: {window_length_of_years}, docs limit: {num_initial_docs}, in {market}")    
+    from query_data import get_chain
+    return get_chain(get_vectorstore(market), window_length_of_years, num_initial_docs)
+
+def get_response(chat_history: list[str]):
+    from constants import MODEL
+    [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+
+    res = openai.ChatCompletion.create(
+    model=MODEL,
+    messages=chat_history
+    )
+    return res['choices'][0]['message']['content']
+
+
+
+def get_chat_response(question, window_length_of_years, num_initial_docs, market):
+    """
+    Return answer and source
+    """
+    qa_chain = get_qa_chain(market, window_length_of_years, num_initial_docs)
+    result = qa_chain({"question": question, "chat_history": st.session_state.chat_history})
+    #chat_history.append((question, result["answer"]))
+    return result["answer"], result['source_documents']
+
+    #if True:
+    #    print(f"Above result is from documents: {[PureWindowsPath(doc.metadata['source']).parts[-1]  for doc in result['source_documents']]}\n")
+        
+    #class Document(BaseModel):
+    """
+    Document
+
+    page_content: str
+    metadata: dict = Field(default_factory=dict)
+    """
+
+    """
+    #first rephrase the question with context
+    if len(st.session_state.chat_history)==0:
+        rephrased_question = question
+    else:
+        rephrased_question = get_response([chatcx_rephrase_message, st.session_state.chat_history])
+
+
+    retriever.get_relevant_documents(rephrased_question)
+
+    get_response([chatcx_system_message, *chat_history])
+    """
+    # Gotta rephrase. Better use the old way. 
--- Front End OCBC/chatbot.py
+++ Front End OCBC/chatbot.py
@@ -1,79 +1,85 @@
 #to run this
 # hide the three dots setting list
 # streamlit run <script_name> --server.port <your port> --client.toolbarMode minimal --browser.gatherUsageStats False
-# streamlit run chatbot.py --server.port 4000 --client.toolbarMode minimal --browser.gatherUsageStats False
-# forever start -c "streamlit run" chatbot.py --server.port 4000 --client.toolbarMode minimal --browser.gatherUsageStats False
+# streamlit run "Front End OCBC/chatbot.py" --server.port 4000 --client.toolbarMode minimal --browser.gatherUsageStats False
+# forever start -c "streamlit run" "Front End OCBC/chatbot.py" --server.port 4000 --client.toolbarMode minimal --browser.gatherUsageStats False
 import streamlit as st
-import pandas as pd
-from util import hide_footer
-from PIL import Image
+from util import *
+
 import json
+from os import path
 
 
+if "relative_dir_name" not in st.session_state:
+    st.session_state.relative_dir_name = path.basename(path.dirname(__file__) )
 
 def heading():
     st.set_page_config(
-        page_title="WealthCX Chatbot V1",
+        page_title="WealthCX Chatbot V2",
         # 👋
-        page_icon=r"icon.png",
+        page_icon=path.join(st.session_state.relative_dir_name,"icon.png"),
         menu_items={
         'About': "# A chatbot for financial statements/documents"
         }
         )
     st.title('ChatCX')
-
-
+    
 def state_init():
     if "chat_history" not in st.session_state:
-        st.session_state.chat_history = ["I am here to query to ask for your help.","Nice to meet you"]
-
-    
-    
+        st.session_state.chat_history = []    
 
 def body():
     col1, col2 = st.columns(2)
     with col1:
         market = st.selectbox("Select market:", ["HKG", "SGP","US","all"]).lower()
-
+    
     with col2:
-        with open("stocks.json","r") as f:
+        with open(path.join(st.session_state.relative_dir_name,"stocks.json"),"r") as f:
             stocks = json.load(f)
             if market == "all":
                 ric = st.selectbox("Stock:", sorted([val for vals in stocks.values() for val in vals]))
             else:
                 ric = st.selectbox("Stock:", sorted(stocks[market]))
 
-    st.caption(f"Selected stock: {ric}")
+    #window_length_of_years, num_initial_docs
+    col1, col2 = st.columns(2)
+    with col1:
+        #window_length_of_years, num_initial_docs
+        window_len = st.slider(":rainbow[Window size of years]", min_value=1, max_value=4, value=1, help='If set to N, then will look into "latest N years" among search results')
+    with col2:
+        doc_num= st.slider(":rainbow[Max limit of documents used]", min_value=1, max_value=8, value=4, 
+            help='If set to N, will at most use N documents: with total token under model limits')
+
+    st.caption(f"✅Selected stock: **:rainbow[{ric}]**")
     
 
       # 👈 Draw the string 'x' and then the value of x
     
     with st.expander("Chat History", expanded=True):
-        for ind, item in enumerate(st.session_state.chat_history):
+        for item in st.session_state.chat_history:
             # Human turn
-            if ind % 2 == 0:
-                st.warning(item, icon=st.session_state.avatar)
+            st.warning(item[0], icon=st.session_state.avatar)
             # bots turn
-            else:
-                st.success(item,icon="🤖")
-            
+            st.success(item[1],icon="🤖")
 
     user_input = st.text_area("Your turn", help="Type your message. Click 'Chat' to get response")
 
     def chat_handler():
-        st.session_state.chat_history.append(user_input)
-
+        if user_input.strip() == '':
+            st.warning('Empty input not allowed', icon="⚠️")
+            return
+        # add plain question to chat_history, to avoid long context
         # chat through openai
-
-        #add response to history
-        st.session_state.chat_history.append("My answer expected to deliver in this week")
+        response, sources = get_chat_response(user_input, window_len, doc_num, market)
+        # add response to history
+        st.session_state.chat_history.append((user_input, response))
 
     _, chat, reset = st.columns([0.8,1,1])
     with chat:
-        st.button('Chat', on_click=chat_handler)  
+        st.button('Chat', on_click=chat_handler)
     with reset:
         st.button('Reset', on_click= lambda: st.session_state.chat_history.clear())
-        
+
         
         
 
@@ -83,6 +89,7 @@ def side_bar():
     st.sidebar.text_input("Your name", key="user_name")
 
 heading()
+from chat_core import *
 side_bar()
 state_init()
 hide_footer()
