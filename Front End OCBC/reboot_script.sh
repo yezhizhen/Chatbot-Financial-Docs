80ec64d97e5214476109cd5045094f89e8a416b2
Euclid <yezhizhenjiakang@gmail.com>
2023-11-03 17:46:21 +0800

    assets + nginx config + server domain change

--- Front End OCBC/reboot_script.sh
+++ Front End OCBC/reboot_script.sh
@@ -5,5 +5,7 @@
 #sudo service nginx start  || or use 
 #sudo systemctl enable nginx.service
 cd ~/sang/processing_code
-forever start -c "python3 -m streamlit run" "Front End OCBC/chatbot.py"
+forever start -c "python3 -m streamlit run" "Front End OCBC/chatbot.py" --server.port 4000
 forever start -c python3 gptv2.py
+cd ~/prototypes/GraphCX
+forever start -c "python3 -m streamlit run" "graphcx.py" --server.port 4001
